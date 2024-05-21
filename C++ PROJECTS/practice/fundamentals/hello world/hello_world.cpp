00727050c07b76e40af3e8b18823cab313f25fb2
AquaInfinite <aquainfinite64@gmail.com>
2024-05-21 19:40:36 -0400

    got c+ project to run

--- C++ PROJECTS/practice/fundamentals/hello world/hello_world.cpp
+++ C++ PROJECTS/practice/fundamentals/hello world/hello_world.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
-using namespace std;
 
 int main() {
   //this is a comment
   /*this is a multiline
   quote*/
-  cout << "Hello World! \n\n";
-  cout << "I am learning C++";
+  std::cout << "Hello World!" << '\n';
   return 0;
 }
