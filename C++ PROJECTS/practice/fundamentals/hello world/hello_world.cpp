58efef039beeafb4a8716f523e620f4915c2e4b0
AquaInfinite <aquainfinite64@gmail.com>
2024-05-21 21:30:10 -0400

    c++ fundamentals practice on namespaces

--- C++ PROJECTS/practice/fundamentals/hello world/hello_world.cpp
+++ C++ PROJECTS/practice/fundamentals/hello world/hello_world.cpp
@@ -1,9 +1,13 @@
+//declares objects that control reading from and writing to the 
+//standard streams
 #include <iostream>
 
 int main() {
   //this is a comment
   /*this is a multiline
   quote*/
-  std::cout << "Hello World!" << '\n';
+  //prints text
+  std::cout << "Hello World!" << "\n";
+  //its an int funtion so it has to return an int
   return 0;
 }
