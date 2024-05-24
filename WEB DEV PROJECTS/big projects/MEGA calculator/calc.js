b11650e93539eb3d41a967c1ad33b120c2df1084
AquaInfinite <aquainfinite64@gmail.com>
2024-05-23 22:01:48 -0400

    worked on mega calculator, can't get showing things with buttons to work

---
+++ WEB DEV PROJECTS/big projects/MEGA calculator/calc.js
@@ -0,0 +1,39 @@
+//hiding all elements off the start
+document.getElementById("secondaryButtons").style.display = "none";
+//gets geometry button element
+const geoB = document.getElementById("geometryButton");
+
+//function for clicking the button
+geoB.addEventListener("click", function(e) {
+    document.getElementById("primaryButtons").style.display = "none";
+    console.log("geometry button pressed!");
+});
+
+//gets geometry button element
+const pythagB = document.getElementById("pythagoreanButton");
+
+//function for clicking the button
+pythagB.addEventListener("click", function(e) {
+    document.getElementById("primaryButtons").style.display = "none";
+    document.getElementById("pythagoreanStuff").style.display = "block";
+    console.log("pythagorean button pressed!");
+});
+
+//gets geometry button element
+const algeB = document.getElementById("algebraButton");
+
+//function for clicking the button
+algeB.addEventListener("click", function(e) {
+    document.getElementById("primaryButtons").style.display = "none";
+    console.log("algebra button pressed!");
+});
+
+//gets go home button element
+const homeB = document.getElementById("homeButton");
+
+//function for clicking the button
+homeB.addEventListener("click", function(e) {
+    document.getElementById("secondaryButtons").style.display = "none";
+    document.getElementById("primaryButtons").style.display = "block";
+    console.log("home button pressed!");
+});
